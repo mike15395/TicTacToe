2e6dfd4f031e5521219aef43fbd7264c64335364
Mikhil Desai <mikhildesai@gmail.com>
2020-06-11 22:47:48 +0530

    Welcome message and 1d array initialized

--- ttt.sh
+++ ttt.sh
@@ -1,1 +1,13 @@
+#!/bin/bash -x
 
+echo "Welcome to TIC-TAC-TOE Game!"
+arr=(- - - - - - - - -) # intialized empty array with 9 elements
+
+function display_matrix()
+{
+	echo "${arr[0]} ${arr[1]} ${arr[2]}"
+	echo "${arr[3]} ${arr[4]} ${arr[5]}"
+	echo "${arr[6]} ${arr[7]} ${arr[8]}"
+}
+
+display_matrix
