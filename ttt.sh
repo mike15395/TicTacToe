291bff01a612138d8d6d2f99ec14fbe7e98eef2a
Mikhil Desai <mikhildesai@gmail.com>
2020-06-12 09:26:16 +0530

    input functionality addedand 2player added for testing

--- ttt.sh
+++ ttt.sh
@@ -5,9 +5,47 @@ arr=(- - - - - - - - -) # intialized empty array with 9 elements
 
 function display_matrix()
 {
-	echo "${arr[0]} ${arr[1]} ${arr[2]}"
-	echo "${arr[3]} ${arr[4]} ${arr[5]}"
-	echo "${arr[6]} ${arr[7]} ${arr[8]}"
+	echo "${arr[0]}  ${arr[1]}  ${arr[2]}"
+	echo "${arr[3]}  ${arr[4]}  ${arr[5]}"
+	echo "${arr[6]}  ${arr[7]}  ${arr[8]}"
+}
+
+
+function input()
+{
+	echo "Enter Position"
+	read position
+	echo "Enter symbol"
+	read sym
+
+	if (( $position >=0 && $position <=8  ))
+	then
+			arr[$position]=$sym
+	else
+		 	echo "Wrong position"
+	fi
+
 }
 
 display_matrix
+
+function play()
+{
+	flag=1
+	if ((flag == 1)) #player1
+	then
+		input
+		display_matrix
+		flag=0
+	fi
+
+	if (( flag == 0)) #player2
+	then
+		input
+		display_matrix
+		flag=1
+	fi
+
+}
+
+play
