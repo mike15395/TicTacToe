b49bc488f16cc388669f512cdc88f998bb900416
Mikhil Desai <mikhildesai@gmail.com>
2020-06-13 00:18:18 +0530

    ADDED array to store all moves

--- ttt.sh
+++ ttt.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
 echo "Welcome to TIC-TAC-TOE Game!"
+declare -a arr
+declare -a his_hum
+declare -a his_com
 arr=(- - - - - - - - -) # intialized empty array with 9 elements
 
 
@@ -16,7 +19,7 @@ function display_matrix() #function to display board
 	echo "${arr[6]}  ${arr[7]}  ${arr[8]}"
 }
 
-
+c1=0
 function input()		#function to accept user input
 {
 
@@ -25,24 +28,29 @@ function input()		#function to accept user input
 
 	if [[ ${arr[$pos_hum]} == "-" ]];
 	then
-
 				arr[$pos_hum]=$sym
+				((count++))
+				echo "Player 1 Entered " $sym "at cell " $pos_hum
+				his_hum[c1++]=$pos_hum #array to store all moves of human
 	else
 		 	echo "Wrong position"
 	fi
 
 }
 
-function computerinput()
+c2=0
+function computerinput()	#computer inputs using RANDOM function
 {
-	#while (( pos_hum != pos_com ))
-	#do
+
 		pos_com=$[(RANDOM%9)]
-	#done
+
 	if [[ ${arr[$pos_com]} == "-" ]];
 	then
 
 				arr[$pos_com]=$sym
+				((count++))
+				echo "COMPUTER Entered "$sym "at cell" $pos_com
+				his_com[c2++]=$pos_com	#array to store all moves of computer
 	else
 		 	echo "Wrong position"
 	fi
@@ -92,8 +100,7 @@ function main
 		then
 			echo "Player 1(x)"		#game starts with displaying board
 			sym=$player1symbol		#then player input their symbol
-			input
-			((++count))							#after each input checking win condition
+			input							#after each input checking win condition
 			wincheck
 				if (( game ==0 ))		#if  won then display it
 				then
@@ -105,6 +112,7 @@ function main
 			flag=0
 		fi
 
+		echo " "
 
 		if (( $count == 9)) #if players chance equal number of cells(9)
 									#then game is draw
@@ -118,7 +126,6 @@ function main
 			echo "COMPUTER (o)"
 			sym=$player2symbol
 			computerinput
-			((++count))
 			wincheck
 					if (( game == 0 ))
 					then
@@ -129,8 +136,9 @@ function main
 			display_matrix
 			flag=1
 		fi
-
-
 	done
+
+	echo "Player 1 Moves" ${his_hum[@]}
+	echo "COMPUTER Moves" ${his_com[@]}
 }
 main
